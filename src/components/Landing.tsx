e0b3183013b94a3f3d28becbf1969096ac4a2440
Matthew Garcia <gmatthew2765@gmail.com>
2025-03-12 22:58:04 -0700

    imported webp.

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+import computer from "@/public/computer.webp";
+import Link from "next/link";
 export default function Landing() {
   return (
     <div className="w-screen">
-      <h1>Hello, I am Matthew</h1>
-      <p>I am an aspiring software engineer</p>
+      <Image src={computer} width={500} height={500} alt="Mr. Iby" />
+      <h1>Frontend Developer</h1>
+      <p>
+        Hi, I’m Matthew, a passionate software developer specializing in
+        frontend web development. With over a year of experience, I create
+        beautiful, user-friendly, and responsive websites. I focus on building
+        high-quality front-end experiences that help bring ideas to life.
+      </p>
     </div>
   );
 }
