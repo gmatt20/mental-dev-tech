70ab7aba5c79d302c1b5f4874a8627baedfbdb17
Matthew Garcia <gmatthew2765@gmail.com>
2025-03-12 21:56:05 -0700

    cleaning directorie location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Header from "./components/Header";
-import Landing from "./components/Landing";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Experience from "./components/Experience";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Header from "../components/Header";
+import Landing from "../components/Landing";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Experience from "../components/Experience";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
