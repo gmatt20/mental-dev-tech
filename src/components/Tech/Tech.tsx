bb3303279de228957859e85ce220bf324d9be6c3
Matthew Garcia <gmatthew2765@gmail.com>
2025-03-13 22:17:15 -0700

    implemented tool tip and motion to caurosel

--- src/components/Tech/Tech.tsx
+++ src/components/Tech/Tech.tsx
@@ -18,10 +18,11 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { motion } from "motion/react";
 
 export default function Tech() {
   return (
-    <div className="flex items-center justify-center mb-10 bg-darker p-10">
+    <div className="flex items-center justify-center mb-10 bg-darker p-14">
       {" "}
       <TooltipProvider delayDuration={100} skipDelayDuration={0}>
         <Slider
@@ -32,8 +33,10 @@ export default function Tech() {
           blurBorderColor={"#fff"}>
           <Slider.Slide>
             <Tooltip>
-              <TooltipTrigger>
-                <SiNextdotjs size={50} />
+              <TooltipTrigger asChild>
+                <motion.div whileHover={{ scale: 1.2 }}>
+                  <SiNextdotjs className="p-2" size={70} />
+                </motion.div>
               </TooltipTrigger>
               <TooltipContent>
                 <p>Next.js</p>
@@ -43,8 +46,10 @@ export default function Tech() {
 
           <Slider.Slide>
             <Tooltip>
-              <TooltipTrigger>
-                <SiTypescript color="#3178C6" size={50} />
+              <TooltipTrigger asChild>
+                <motion.div whileHover={{ scale: 1.2 }}>
+                  <SiTypescript className="p-2" color="#3178C6" size={70} />
+                </motion.div>
               </TooltipTrigger>
               <TooltipContent>
                 <p>TypeScript</p>
@@ -54,8 +59,10 @@ export default function Tech() {
 
           <Slider.Slide>
             <Tooltip>
-              <TooltipTrigger>
-                <SiHtml5 color="#E34F26" size={50} />
+              <TooltipTrigger asChild>
+                <motion.div whileHover={{ scale: 1.2 }}>
+                  <SiHtml5 className="p-2" color="#E34F26" size={70} />
+                </motion.div>
               </TooltipTrigger>
               <TooltipContent>
                 <p>HTML5</p>
@@ -65,8 +72,10 @@ export default function Tech() {
 
           <Slider.Slide>
             <Tooltip>
-              <TooltipTrigger>
-                <SiCss color="#663399" size={50} />
+              <TooltipTrigger asChild>
+                <motion.div whileHover={{ scale: 1.2 }}>
+                  <SiCss className="p-2" color="#663399" size={70} />
+                </motion.div>
               </TooltipTrigger>
               <TooltipContent>
                 <p>CSS</p>
@@ -76,8 +85,10 @@ export default function Tech() {
 
           <Slider.Slide>
             <Tooltip>
-              <TooltipTrigger>
-                <SiJavascript color="#F7DF1E" size={50} />
+              <TooltipTrigger asChild>
+                <motion.div whileHover={{ scale: 1.2 }}>
+                  <SiJavascript className="p-2" color="#F7DF1E" size={70} />
+                </motion.div>
               </TooltipTrigger>
               <TooltipContent>
                 <p>JavaScript</p>
@@ -87,8 +98,10 @@ export default function Tech() {
 
           <Slider.Slide>
             <Tooltip>
-              <TooltipTrigger>
-                <SiTailwindcss color="#06B6D4" size={50} />
+              <TooltipTrigger asChild>
+                <motion.div whileHover={{ scale: 1.2 }}>
+                  <SiTailwindcss className="p-2" color="#06B6D4" size={70} />
+                </motion.div>
               </TooltipTrigger>
               <TooltipContent>
                 <p>Tailwind CSS</p>
@@ -98,8 +111,10 @@ export default function Tech() {
 
           <Slider.Slide>
             <Tooltip>
-              <TooltipTrigger>
-                <SiCplusplus color="#00599C" size={50} />
+              <TooltipTrigger asChild>
+                <motion.div whileHover={{ scale: 1.2 }}>
+                  <SiCplusplus className="p-2" color="#00599C" size={70} />
+                </motion.div>
               </TooltipTrigger>
               <TooltipContent>
                 <p>C++</p>
@@ -109,8 +124,10 @@ export default function Tech() {
 
           <Slider.Slide>
             <Tooltip>
-              <TooltipTrigger>
-                <SiGit color="#F05032" size={50} />
+              <TooltipTrigger asChild>
+                <motion.div whileHover={{ scale: 1.2 }}>
+                  <SiGit className="p-2" color="#F05032" size={70} />
+                </motion.div>
               </TooltipTrigger>
               <TooltipContent>
                 <p>Git</p>
@@ -120,8 +137,10 @@ export default function Tech() {
 
           <Slider.Slide>
             <Tooltip>
-              <TooltipTrigger>
-                <SiGithub size={50} />
+              <TooltipTrigger asChild>
+                <motion.div whileHover={{ scale: 1.2 }}>
+                  <SiGithub className="p-2" size={70} />
+                </motion.div>
               </TooltipTrigger>
               <TooltipContent>
                 <p>GitHub</p>
