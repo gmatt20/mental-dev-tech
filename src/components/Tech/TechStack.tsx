c9a64577ef3483196dfcc68a9c1f0263bb53a979
Matthew Garcia <gmatthew2765@gmail.com>
2025-03-15 15:05:56 -0700

    made techstack component cleaner

--- src/components/Tech/TechStack.tsx
+++ src/components/Tech/TechStack.tsx
@@ -1,23 +1,18 @@
 import { Syne } from "next/font/google";
-import {
-  SiNextdotjs,
-  SiTypescript,
-  SiJavascript,
-  SiExpress,
-  SiMongodb,
-  SiMongoose,
-  SiPostman,
-  SiTailwindcss,
-  SiCplusplus,
-  SiGit,
-  SiGithub,
-} from "@icons-pack/react-simple-icons";
 import { TechData } from "@/data/TechData";
-import Tech from "./Tech";
 const syne = Syne({
   weight: "800",
   subsets: ["sans"],
 });
+
+const renderTechStack = (techArr) => {
+  return techArr.map((tech, id) => (
+    <div key={id}>
+      <tech.component color={tech.color} className="p-2" size={70} />
+      <p>{tech.name}</p>
+    </div>
+  ));
+}
 
 export default function TechStack() {
   const filteredFrontend = TechData.filter(
@@ -38,64 +33,31 @@ export default function TechStack() {
     (tech) => tech.name === "Git" || tech.name === "GitHub"
   );
   const filteredLearningCs = TechData.filter((tech) => tech.name === "C++");
+  
+  const techCategories = [
+    { title: "FRONTEND", data: filteredFrontend },
+    { title: "BACKEND (CURRENTLY LEARNING)", data: filteredBackend },
+    { title: "VERSION CONTROL", data: filteredVersionControl },
+    { title: "LEARNING CS", data: filteredLearningCs },
+  ];
 
   return (
     <div className="my-10">
       <h1 className={`${syne.className} text-5xl mb-5 text-center`}>
         MY TECH STACK
       </h1>
-      <div className="bg-darker flex flex-col items-center py-10 rounded-lg my-5">
-        <h1 className={`${syne.className} text-5xl mb-5 text-center`}>
-          FRONTEND
-        </h1>
-        <div className="grid grid-cols-3 gap-5 place-items-center text-center">
-          {filteredFrontend.map((tech, id) => (
-            <div key={id}>
-              <tech.component color={tech.color} className="p-2" size={70} />
-              <p>{tech.name}</p>
+      {techCategories.map((category, id) => (
+        <div key={id}>
+          <div className="bg-darker flex flex-col items-center py-10 rounded-lg my-5">
+            <h1 className={`${syne.className} text-5xl mb-5 text-center`}>
+              {category.title}
+            </h1>
+            <div className="grid grid-cols-3 gap-5 place-items-center text-center">
+              {renderTechStack(category.data)}
             </div>
-          ))}
+          </div>
         </div>
-      </div>
-      <div className="bg-darker flex flex-col items-center py-10 rounded-lg my-5">
-        <h1 className={`${syne.className} text-5xl mb-5 text-center`}>
-          BACKEND (CURRENTLY LEARNING)
-        </h1>
-        <div className="grid grid-cols-3 gap-5 place-items-center text-center">
-          {filteredBackend.map((tech, id) => (
-            <div key={id}>
-              <tech.component color={tech.color} className="p-2" size={70} />
-              <p>{tech.name}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="bg-darker flex flex-col items-center py-10 rounded-lg my-5">
-        <h1 className={`${syne.className} text-5xl mb-5 text-center`}>
-          VERSION CONTROL
-        </h1>
-        <div className="grid grid-cols-3 gap-5 place-items-center text-center">
-          {filteredVersionControl.map((tech, id) => (
-            <div key={id}>
-              <tech.component color={tech.color} className="p-2" size={70} />
-              <p>{tech.name}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="bg-darker flex flex-col items-center py-10 rounded-lg my-5">
-        <h1 className={`${syne.className} text-5xl mb-5 text-center`}>
-          LEARNING CS
-        </h1>
-        <div className="grid grid-cols-3 gap-5 place-items-center text-center">
-          {filteredLearningCs.map((tech, id) => (
-            <div key={id}>
-              <tech.component color={tech.color} className="p-2" size={70} />
-              <p>{tech.name}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
