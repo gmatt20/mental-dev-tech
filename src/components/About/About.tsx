3e1966a0fca27620903df7ca667a637a88749334
Matthew Garcia <gmatthew2765@gmail.com>
2025-03-15 15:55:40 -0700

    reworking about me.

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -17,7 +17,7 @@ export default function About() {
       </h1>
       <div className="flex flex-col items-center md:flex-row gap-5">
         <p className="md:w-[70%]">
-          My journey into software development began while studying psychology
+          My journey into software development started while studying psychology
           at the{" "}
           <Link
             href="https://www.ucr.edu/"
@@ -27,56 +27,54 @@ export default function About() {
               University of California, Riverside
             </p>
           </Link>
-          . During my exploration of different subjects, I took my first formal
-          computer science course, where I learned C++ programming. The
-          creativity and logic behind coding instantly fascinated me, but at the
-          time, my focus was on becoming a psychologist.
+          . While exploring different subjects, I took my first formal computer
+          science course in C++, sparking my interest in coding. Though I was
+          initially focused on becoming a psychologist, the creativity and logic
+          of programming captivated me.
         </p>
         <div className="mb-10 w-[80%] md:w-[50%] flex flex-col items-center bg-darker mt-10 p-10 rounded-lg">
           <Image
             src={UCR}
-            alt="picture of UC Riverside"
+            alt="UC Riverside"
             className="rounded-lg mx-0"
             width={500}
             height={500}
           />
           <p className="text-sm">
-            Fun fact: I’m currently majoring in psychology, but I’m also taking
-            computer science classes to deepen my formal knowledge in the field.
-            It’s been an exciting way to blend my passion for human behavior
-            with my growing interest in tech!
+            Fun fact: I'm currently majoring in psychology but also taking
+            computer science courses to deepen my knowledge. It’s an exciting
+            way to merge my passion for human behavior with my growing interest
+            in tech!
           </p>
         </div>
       </div>
       <div className="flex flex-col items-center md:flex-row-reverse gap-5">
         <p className="md:w-[70%]">
-          Despite not pursuing additional CS courses initially, I developed a
-          passion for programming and began teaching myself HTML, CSS, and
-          JavaScript as a hobby. However, everything changed in the summer of
-          2024, when I decided to take programming seriously. I dove deep into
-          web development, learning through freeCodeCamp, The Odin Project,
-          documentation, and YouTube tutorials. That summer, I realized my true
-          passion was in software engineering.
+          Although I didn’t pursue additional CS courses at first, I began
+          teaching myself HTML, CSS, and JavaScript as a hobby. Everything
+          changed in the summer of 2024 when I decided to focus on programming
+          full-time. I immersed myself in web development, learning through
+          freeCodeCamp, The Odin Project, documentation, and YouTube tutorials.
+          That summer, I realized software engineering was my true passion.
         </p>
         <div className="mb-10 w-[80%] md:w-[50%] flex flex-col items-center bg-darker mt-10 p-10 rounded-lg">
           <Image
             src={SanFrancisco}
-            alt="picture of UC Riverside"
+            alt="San Francisco"
             className="rounded-lg mx-0"
             width={500}
             height={500}
           />
           <p className="text-sm">
-            Fun fact: A road trip to San Francisco in the summer of 2024
-            reinforced my desire to learn software engineering. It made me
-            realize that this was the field I truly wanted to dive into.
+            Fun fact: A road trip to San Francisco in 2024 solidified my desire
+            to pursue software engineering. It helped me realize this was the
+            field I wanted to dive into.
           </p>
         </div>
       </div>
       <p className="mb-10">
         Since then, I’ve completed formal computer science courses, including
-        Object-Oriented Programming in C++ at UCR, and I’m currently pursuing
-        the{" "}
+        Object-Oriented Programming in C++ at UCR, and am currently pursuing the{" "}
         <Link
           href="https://www.coursera.org/professional-certificates/ibm-full-stack-cloud-developer"
           rel="noopener noreferrer"
@@ -85,7 +83,7 @@ export default function About() {
             IBM Full Stack Developer Specialization on Coursera
           </p>
         </Link>
-        . I’ve also been actively involved in my university’s{" "}
+        . I’m also involved in my university’s{" "}
         <Link
           href="https://acm.cs.ucr.edu/"
           rel="noopener noreferrer"
@@ -103,7 +101,7 @@ export default function About() {
             Taekwondo Club at UCR
           </p>
         </Link>
-        . Additionally, I gained hands-on experience in collaborative software
+        . I also gained hands-on experience in collaborative software
         development by participating in{" "}
         <Link
           href="https://cutiehack.com/"
@@ -116,10 +114,10 @@ export default function About() {
         , a hackathon hosted by my university.
       </p>
       <p className="mb-10">
-        I aspire to become a frontend software engineer and thrive in a
-        collaborative engineering team where I can contribute, learn, and grow
-        alongside other developers. I love working in a team setting, seeing
-        projects evolve, and building scalable applications that make an impact.
+        I aspire to become a frontend software engineer, thrive in a
+        collaborative engineering team, and build scalable applications that
+        make an impact. I love working with a team, seeing projects evolve, and
+        contributing to meaningful software solutions.
       </p>
     </div>
   );
