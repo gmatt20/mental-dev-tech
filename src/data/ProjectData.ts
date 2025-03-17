fcd7c4134d9549de12240bba591b0a5d366bad00
Matthew Garcia <gmatthew2765@gmail.com>
2025-03-16 20:28:18 -0700

    imported project photos into data file

---
+++ src/data/ProjectData.ts
@@ -0,0 +1,38 @@
+import mockWeb from "@/public/mockWeb.webp";
+import notes from "@/public/notes.webp";
+import odin from "@/public/odin.webp";
+import origMDT from "@/public/origMDT.webp";
+import psycWeb from "@/public/psycWeb.webp";
+import rpool from "@/public/rpool.webp";
+import tkd from "@/public/tkd.webp";
+
+export const ProjectData = [
+  {
+    url: mockWeb,
+    alt: "Picture of mock website",
+  },
+  {
+    url: notes,
+    alt: "Picture of full stack notes web application coming soon",
+  },
+  {
+    url: odin,
+    alt: "Picture of Odin Landing Page",
+  },
+  {
+    url: origMDT,
+    alt: "Picture of first Mental Dev Tech website",
+  },
+  {
+    url: psycWeb,
+    alt: "Picture of mock psychologist website",
+  },
+  {
+    url: rpool,
+    alt: "Picture of Cutiehack MVP collaborated with a team",
+  },
+  {
+    url: tkd,
+    alt: "Picture of The Taekwondo @ UCR Website",
+  },
+];
