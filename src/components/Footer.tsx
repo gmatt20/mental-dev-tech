3e19227e46df3160ada63d4c17c5e18e309dbbf1
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-01 19:17:45 -0700

    added github and linkedin links to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,16 @@
 import { BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="bg-darker max-md:flex-col flex justify-evenly items-center p-10 font-syne">
+    <div className="bg-darker max-md:flex-col flex justify-evenly items-center p-10">
       <div className="max-md:mb-10">
-        <h1 className="text-3xl text-center uppercase">Matthew Garcia</h1>
+        <h1 className="text-xl uppercase font-syne text-left max-md:text-center">
+          Matthew Garcia
+        </h1>
       </div>
-      <div className="font-syne max-md:mb-10">
+      <div className="max-md:mb-10">
         <ul className="gap-6 flex-col items-center justify-center flex ">
           <li className="text-center hover:bg-hover hover:cursor-pointer hover:duration-150 hover:ease-in rounded-md active:bg-active">
             HOME
@@ -22,9 +26,19 @@ export default function Footer() {
           </li>
         </ul>
       </div>
-      <div>
-        <p>Get to know me:</p>
-        <BsLinkedin />
+      <div className="flex items-center gap-5">
+        <Link
+          href="https://www.linkedin.com/in/mattg2765/"
+          rel="noopener noreferrer"
+          target="_blank">
+          <BsLinkedin size={50} />
+        </Link>
+        <Link
+          href="https://github.com/gmatt20"
+          rel="noopener noreferrer"
+          target="_blank">
+          <FaGithubSquare size={58} />
+        </Link>
       </div>
     </div>
   );
