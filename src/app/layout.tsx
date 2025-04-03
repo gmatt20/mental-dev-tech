f9a9ffd4ff41deedaebbb1cfd5a7c6919a79177c
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-03 00:36:00 -0700

    fixed animation overflow bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className} bg-dark text-text`}>
+      <body className={`${montserrat.className} bg-dark text-text overflow-x-hidden`}>
         <Nav />
         <div className="mx-10">{children}</div>
         <Footer />
