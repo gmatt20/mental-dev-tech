2a36d7c86e42040671773a0813a2a20c5b222a81
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-03 16:52:50 -0700

    fixed inspo image mobile responsiveness

--- src/components/Inspo.tsx
+++ src/components/Inspo.tsx
@@ -3,16 +3,14 @@ import Image from "next/image";
 
 export default function Inspo() {
   return (
-    <div className="w-screen flex justify-center items-center flex-col mb-10">
-      <div className="w-[60%]">
+    <div className="flex justify-center items-center flex-col mb-10">
+      <div className="w-full">
         <Image
           src={Superman}
           alt="Photo of superman"
-          className="rounded-lg mx-0"
-          width={1000}
-          height={1000}
+          className="rounded-lg mx-0 max-w-[100%]"
         />
-        <h1 className="text-2xl leading-9 md:text-5xl ">
+        <h1 className="text-xl leading-9 md:text-5xl">
           &quot;The only way to do great work is to love what you do.&quot;
         </h1>
         <p>- Steve Jobs</p>
