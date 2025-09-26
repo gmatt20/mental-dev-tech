8e0829eb933eceefe9ee6672f0e9273d77836406
Matthew Garcia <gmatthew2765@gmail.com>
2025-09-26 10:33:05 -0700

    removed projects

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -1,10 +1,4 @@
 import { StaticImageData } from "next/image";
-import mockWeb from "@/public/mockWeb.webp";
-import notes from "@/public/notion-clone.webp";
-import odin from "@/public/odin.webp";
-import origMDT from "@/public/origMDT.webp";
-import psycWeb from "@/public/psycWeb.webp";
-import rpool from "@/public/rpool.webp";
 import tkd from "@/public/tkd.webp";
 import acm from "@/public/acm.webp";
 import aisc from "@/public/aisc.webp";
@@ -17,12 +11,6 @@ interface Project {
 }
 
 export const ProjectData: Project[] = [
-  {
-    url: rpool,
-    link: "https://r-pool.vercel.app/",
-    title: "R'Pool: Cutiehack Collaborative MVP Application",
-    alt: "Picture of Cutiehack MVP collaborated with a team",
-  },
   {
     url: tkd,
     link: "https://tkd.ucrhighlanders.org/",
@@ -40,35 +28,5 @@ export const ProjectData: Project[] = [
     link: "https://aisc.ucrhighlanders.org/",
     title: "AI Student Collective @ UCR",
     alt: "Picture of the AI Student Collective @ UCR Website"
-  },
-  {
-    url: notes,
-    link: "https://github.com/gmatt20/notion-clone",
-    title: "Fullstack Notion Clone Application",
-    alt: "Picture of full stack notes web application coming soon",
-  },
-  {
-    url: origMDT,
-    link: "https://gmatt20.github.io/personal-website/",
-    title: "Mental Dev Tech Original Website",
-    alt: "Picture of first Mental Dev Tech website",
-  },
-  {
-    url: mockWeb,
-    link: "https://gmatt20.github.io/mock-website1/",
-    title: "Professional Mock Website",
-    alt: "Picture of mock website",
-  },
-  {
-    url: odin,
-    link: "https://gmatt20.github.io/odin-landing-page/",
-    title: "Odin Landing Page",
-    alt: "Picture of Odin Landing Page",
-  },
-  {
-    url: psycWeb,
-    link: "https://gmatt20.github.io/psychologist-mock-website1/",
-    title: "Professional Mock Psychologist Website",
-    alt: "Picture of mock psychologist website",
   },
 ];
